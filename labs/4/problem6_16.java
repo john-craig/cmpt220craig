dd6cb752b301b9ec38faab1c0f43dd0ff73e751e
Juan Arias <juan.arias@marist.edu>
2017-02-16 23:16:27 -0500

    Feedback on Lab 4

--- labs/4/problem6_16.java
+++ labs/4/problem6_16.java
@@ -1,3 +1,4 @@
+// JA: Always add comments to your code
 public class problem6_16{
 	public static void main(String[] args) {
 		int year = 2000;
@@ -9,7 +10,8 @@ public class problem6_16{
 	}
 	
 	public static int numberOfDays(int year){
-		if(((year % 4) == 0) && ((year % 100) == 0) && ((year % 400) == 0)){return 366;}
+		if(((year % 4) == 0) && ((year % 100) == 0) && ((year % 400) == 0)){return 366;} // JA: This function is wrong
+		// JA: The correct formula is (year % 400 == 0 ) || (year % 4 == 0 && year % 100 != 0) 
 		else{return 365;}
 	}
 }
--- labs/4/problem7_4.java
+++ labs/4/problem7_4.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+// JA: Always add comments to your code
 public class problem7_4{
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
@@ -19,7 +20,7 @@ public class problem7_4{
 			}
 		}
 
-		int average = sum / amt;
+		int average = sum / amt; // JA: This has to be a non-integer calculation
 		int aboveAve = 0;
 		int belowAve = 0;
 		
